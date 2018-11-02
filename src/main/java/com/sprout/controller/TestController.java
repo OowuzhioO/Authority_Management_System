2ffee46e262ceab8865352c2d162008de073bbe9
OowuzhioO <haowenjiang1993@gmail.com>
2018-11-02 10:44:40 -0500

    struc

--- src/main/java/com/sprout/controller/TestController.java
+++ src/main/java/com/sprout/controller/TestController.java
@@ -1,20 +1,21 @@
 package com.sprout.controller;
 
 
-import lombok.extern.slf4j.Slf4j;
+//import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/test")
-@Slf4j
+//@Slf4j
 public class TestController {
 
     @RequestMapping("/hello")
     @ResponseBody
     public String hello() {
-        log.info("hello");
-        return "hekko, permission";
+//        log.info("hello");
+        System.out.println("test successfully");
+        return "hello, permission";
     }
 }
