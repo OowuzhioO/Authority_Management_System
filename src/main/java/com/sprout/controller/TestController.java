3eca040b47a758ca500b1f019f803d9f7f86d6fb
OowuzhioO <haowenjiang1993@gmail.com>
2018-11-01 21:41:28 -0500

    intitial

---
+++ src/main/java/com/sprout/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.sprout.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        log.info("hello");
+        return "hekko, permission";
+    }
+}
